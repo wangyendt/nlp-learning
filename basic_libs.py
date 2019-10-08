6d3fb125280f458ccfeb2fa16c584294b8fb6f59
Ye Wang <905317742@qq.com>
2019-10-09 00:08:41 +0800

    Update basic_libs.py

--- basic_libs.py
+++ basic_libs.py
@@ -18,12 +18,27 @@ import selenium
 from selenium import webdriver
 import pprint
 from bs4 import BeautifulSoup
+from pyquery import PyQuery as pq
+import pymongo
 
 print(urllib.request.urlopen('http://www.baidu.com'))
 print(requests.get('http://www.baidu.com'))
 # driver = webdriver.Chrome()
 # driver.get('http://www.baidu.com')
 # pprint.pprint(driver.page_source)
-driver = webdriver.PhantomJS()
-driver.get('http://www.baidu.com')
-print(driver.page_source)
+# driver = webdriver.PhantomJS()
+# driver.get('http://www.baidu.com')
+# print(driver.page_source)
+
+# soup = BeautifulSoup('<html></html>','lxml')
+# print(soup)
+
+doc = pq('<html>Hello</html>')
+res = doc('html').text()
+print(res)
+
+client = pymongo.MongoClient('localhost')
+db = client['newtestdb']
+db['table'].insert({'name':'Bob'})
+print(db['table'].find_one({'name':'Bob'}))
+
