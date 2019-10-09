38c0b8899cdfdc79123db1da821f5fe1ce4c13c4
Ye Wang <905317742@qq.com>
2019-10-09 18:04:54 +0800

    Update basic_libs.py

--- basic_libs.py
+++ basic_libs.py
@@ -20,9 +20,12 @@ import pprint
 from bs4 import BeautifulSoup
 from pyquery import PyQuery as pq
 import pymongo
+import requests
+from requests.packages import urllib3
+urllib3.disable_warnings()
 
-print(urllib.request.urlopen('http://www.baidu.com'))
-print(requests.get('http://www.baidu.com'))
+# print(urllib.request.urlopen('http://www.baidu.com'))
+# print(requests.get('http://www.baidu.com'))
 # driver = webdriver.Chrome()
 # driver.get('http://www.baidu.com')
 # pprint.pprint(driver.page_source)
@@ -33,12 +36,50 @@ print(requests.get('http://www.baidu.com'))
 # soup = BeautifulSoup('<html></html>','lxml')
 # print(soup)
 
-doc = pq('<html>Hello</html>')
-res = doc('html').text()
-print(res)
+# doc = pq('<html>Hello</html>')
+# res = doc('html').text()
+# print(res)
+#
+# client = pymongo.MongoClient('localhost')
+# db = client['newtestdb']
+# db['table'].insert({'name':'Bob'})
+# print(db['table'].find_one({'name':'Bob'}))
+#
+# response = requests.get('http://www.baidu.com')
+# print(response.text)
+# print(response.headers)
+# print(response.status_code)
+#
+#
+# data = {
+#     'name':'germey',
+#     'age':22
+# }
+# response = requests.get('http://httpbin.org/get',params=data)
+# print(response.text)
+# response = requests.get('http://httpbin.org/get')
+# print(response.json())
+#
+#
+# response = requests.get('https://github.com/favicon.ico')
+# with open('github.ico','wb') as f:
+#     f.write(response.content)
 
-client = pymongo.MongoClient('localhost')
-db = client['newtestdb']
-db['table'].insert({'name':'Bob'})
-print(db['table'].find_one({'name':'Bob'}))
+# headers = {
+#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
+# }
+# response = requests.get('https://www.zhihu.com/explore',headers=headers)
+# print(response.status_code)
 
+
+# s = requests.Session()
+# s.get('http://httpbin.org/cookies/set/hello/world')
+# response = s.get('http://httpbin.org/cookies')
+# print(response.text)
+
+# response = requests.get('https://www.12306.cn',verify=False)
+# print(response.status_code)
+
+# proxies = {
+#     'http'
+# }
